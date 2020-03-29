87da4e59e5b36ce2a6c9e6bb02ffec16e478c7ee
dvranga <devangmranganth>
2020-03-30 01:47:53 +0530

    [ranganath]Added:finally closing the exression with supporting optional parts with user case 5

--- ValidateEmail.sh
+++ ValidateEmail.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 echo enter email to validate
 read mail
-pat="^[abc]"
+pat="^abc[-_+.]{1}?[0-9]{3}?@(bridgelabz|gmail|yahoo|1|abc)(.com|.co|.net)(.au|.in)?$"
 if [[ $mail =~ $pat ]]
 then
 echo yes
