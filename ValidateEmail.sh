938197cd9a30c600b5b39cf26740380c634dedd6
dvranga <62434972+dvranga@users.noreply.github.com>
2020-03-30 15:25:46 +0530

    [ranganath]Added:finally closing the expression with supporting options

--- ValidateEmail.sh
+++ ValidateEmail.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -x
 echo enter email to validate
 read mail
-pat="^abc[-_+.]{1}?[0-9]{3}?@(bridgelabz|gmail|yahoo|1|abc)(.com|.co|.net)(.au|.in)?$"
+pat="^abc[-_+.]{1}?[0-9]{3}?@(bridgelabz|gmail|yahoo|1|abc)(.com|.co|.net|.com.com)(.au|.in)?$"
 if [[ $mail =~ $pat ]]
 then
 echo yes
